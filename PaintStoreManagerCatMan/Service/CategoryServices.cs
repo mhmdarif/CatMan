fea50ed2f7778da39f5314799aba45bb1aba278d
MuhammadArifH <arif.chgamer.ad@gmail.com>
2021-06-09 11:25:18 +0700

    menambahkan interface dan service (category), menambahkan tabel category

--- PaintStoreManagerCatMan/Service/CategoryServices.cs
+++ PaintStoreManagerCatMan/Service/CategoryServices.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PaintStoreManagerCatMan.Entity;
+using PaintStoreManagerCatMan.Service;
+using System.Data.SqlClient;
 
 namespace PaintStoreManagerCatMan.Service
 {
-    class CategoryServices
+    class CategoryServices : ICategorySvs
     {
+        readonly string connstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database\PaintStoreDB.mdf;Integrated Security=True;Connect Timeout=30";
+        
+        public List<Category> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(string catname, string catdesc)
+        {
+            SqlConnection con = new SqlConnection(connstring);
+            string sql = "insert into TblCategories (Catename, CateDesc) values('" + catname"','" +y + "','" + hour + "')";
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, string catname, string catdesc)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ PaintStoreManagerCatMan/Service/ICategorySvs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PaintStoreManagerCatMan.Entity;
+
+namespace PaintStoreManagerCatMan.Service
+{
+    interface ICategorySvs
+    {
+        List<Category> GetAllCategories();
+        void Add(string catname, string catdesc);
+        void Update(int id, string catname, string catdesc);
+        void Delete(int id);
+    }
+}
